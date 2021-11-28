4044fdfd1cde8d4088ed4089067f6fc3c295cd84
Elizabeth <liz47220@gmail.com>
2021-11-28 14:07:19 +0200

    commit

--- lab3/lab3.hs
+++ lab3/lab3.hs
@@ -2,3 +2,47 @@
 --студентки групи КН-31 підгрупи 1
 --Попової Єлизавети
 --Варіант 10
+
+-- Мета: 
+
+-- 1.10 Видалити кожен n-й елемент списку, напр. при n=2: "1234590"⇒"1350".
+
+-- areTheySame :: Int -> Int-> [Int]
+-- areTheySame x y | x == y = []
+--                 | otherwise = [y]
+
+-- -- tailLen :: [a] -> Int
+
+-- removeItem :: Int -> [Int] -> [Int]
+-- removeItem _ [] = []
+-- removeItem x l@(y:ys) | length l < x   = l --если список меньше номера удаления
+--                       | otherwise         = areTheySame x y ++ removeItem x yдs   
+
+
+-- removeItem :: Int -> [Int] -> [Int]
+-- removeItem _ [] = []
+-- removeItem x (y:ys) | length ys < x     = areTheySame x y ++ ys
+--                     | otherwise         = areTheySame x y ++ removeItem x ys   
+-- 2.10 Знайти усi простi числа в указаному дiапазонi.
+
+-- findSimple :: Int -> Int -> [Int]
+-- findSimple x y n 
+
+
+f :: Int -> Bool
+f 1 = False
+f 2 = True
+f n 
+    | length[x | x <- [2 .. n-1], mod n x == 0] > 0 = False
+    | otherwise = True
+
+divis :: Int -> [Int]
+divis n = [x | x <- [1..n], f x, mod n x == 0]
+-- divis_2 15 -> [3,5]
+-- divis_2 49 -> [7]
+
+
+find :: Int -> Int -> [Int]
+find x y | x == y            = []
+         | length (divis (x)) >= 2     = [] ++  find (x+1) y
+         | otherwise         = x : find (x+1) y
--- lab4/lab4.hs
+++ lab4/lab4.hs
@@ -3,6 +3,69 @@
 --Попової Єлизавети
 --Варіант 10
 
-
 -- :load lab4\\lab4.hs
 
+
+-- Публiкацiї. Зберiгаються данi про публiкацiї, якi можуть бути книгою (ав-
+-- тор/спiвавтори, назва, мiсто, видавництво, рiк), статтею (автор/спiвавтори, на-
+-- зва статтi, назва журналу, рiк, номер журналу, сторiнки) або тезами доповiдi
+-- (автор/спiвавтори, назва доповiдi, назва конференцiї, мiсто, рiк, сторiнки). Ви-
+-- значне функцiї для :
+
+-- статистика публiкацiй автора — кiлькiсть, обсяг (у сторiнках), тип;
+
+data Book = Book {
+    authorBook :: String,
+    nameBook :: String,
+    cityBook :: String,
+    published :: String,
+    yearBook :: Int
+} deriving (Eq, Show)
+
+data Article = Article {
+    authorArticle :: String,
+    nameArticle :: String,
+    magazineName :: String,
+    yearArticle :: Int,
+    magazineNumber :: Int,
+    pageNumber :: Int
+} deriving (Eq, Show)
+
+data Thesis = Thesis {
+    authorThesis :: String,
+    nameThesis :: String,
+    nameConference :: String,
+    cityThesis :: String,
+    yearThesis :: Int,
+    pageThesis:: Int
+} deriving (Eq, Show)
+
+
+fBook :: [Book]
+fBook = [(Book "AB" "First Book" "Kiev" "p1" 1982),(Book "BB" "Second Book" "Lviv" "p2" 1999),(Book "CB" "Ftird Book" "Moscow" "p3" 2001) ]
+
+fArticle :: [Article]
+fArticle = [(Article "AA" "First Article" "magazine1" 2021 8 32),(Article "BA" "Second Article" "magazine2" 1986 24 215)]
+
+fThesis :: [Thesis]
+fThesis = [(Thesis "AT" "First Thesis" "conference1" "Kiev" 2018 9)]
+
+applyEach :: [(a -> b)] -> [a] -> [b]
+applyEach _ [] = []
+applyEach (x:xs) (y:ys) = x y : applyEach xs ys
+
+elem' :: String -> [Book] -> String
+elem' _ [] = "False"
+elem' x (y : ys) = if x == authorBook y then nameBook y else elem' x ys
+
+elem2' :: String -> [Article] -> String
+elem2' _ [] = "False"
+elem2' x (y : ys) = if x == authorArticle y then nameArticle y else elem2' x ys
+
+elem3' :: String -> [Thesis] -> String
+elem3' _ [] = "False"
+elem3' x (y : ys) = if x == authorThesis y then nameThesis y else elem3' x ys
+
+check :: String -> [String]
+check a = [elem' a fBook, elem2' a fArticle,elem3' a fThesis]
+    
